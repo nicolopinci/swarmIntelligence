111b58aca96431f741ec1062f4f37491e0627e1b
nicolopinci <nicolopinci.1997@gmail.com>
2020-03-13 13:02:15 +0100

    Increased flexibility in path following

--- wallFollowing.py
+++ wallFollowing.py
@@ -27,23 +27,25 @@ class Agent:
         
         
             
-    def moveTo(self, pos, vel):
+    def moveTo(self, pos, vel, factor, sphere):
                 
         distance = self.calculateDistance(pos)
         
         requiredTime = 0
         
         if(norm(vel) != 0):
-            requiredTime = math.ceil(norm(distance)/norm(vel))
+            requiredTime = math.ceil(norm(distance)/(factor*norm(vel)))
         
         t = 0
         while(t < requiredTime):
             t += 1
             for i in range(0, len(self.pos)):
                 self.pos[i] -= distance[i]/requiredTime
+            self.updateSphere(sphere)
+            time.sleep(0.05)
                 
     def generateSphere(self, rad):
-        return sphere(pos = vector(self.pos[0], self.pos[1], self.pos[2]), radius = rad)
+        return sphere(pos = vector(self.pos[0], self.pos[1], self.pos[2]), radius = rad, color=vector(1, 0, 0))
     
     def updateSphere(self, sphere):
         sphere.pos = vector(self.pos[0], self.pos[1], self.pos[2])
@@ -69,13 +71,44 @@ def addOffset(pos, offset, acivate):
     for i in range(0, len(pos)):
         if(activate[i] == True):
             pos[i] += offset
+
+def obtainOffsetPath(wall, rad, sides, myPos, offset) :
+    return paths.circle(pos=myPos, np=sides, radius=rad+offset)
+        
+def defineNGonalWall(number, sides, rad, center):
+    wallParts = []
+    pathParts = []
+    for n in range(0, number):
+        wallParts.append(paths.circle(radius = rad, np = sides, pos=center + vec(2*rad*n, 0, 0)))
+        pathParts.append(obtainOffsetPath(wallParts[n], rad, sides, center + vec(2*rad*n, 0, 0), 1))
+        
+    wall = []
+    path = []
+    for wp in range(0, len(wallParts)):
+        wall += wallParts[wp]
+        path += pathParts[wp]
+        
+    return [wall, path]
     
-offset = 0.2
+offset = 0.1
 radius = 0.5
 
-wallPath = paths.circle(radius = 2)
-#wallPath = paths.rectangle(width=100, height=8)
+[wallPath, pathPath] = defineNGonalWall(1, 10, 2, vector(0, 0, 0))
+#[secondWallPath, secondPathPath] = defineNGonalWall(1, )
+#exagonWall = paths.circle(radius = 2, np = 6, pos=vec(0, 0, 0))
+#squareWall = paths.circle(radius = 2, np = 4, pos=vec(-4, 0, 0))
+#triangleWall = paths.circle(radius = 4, np = 3, pos=vec(0, 0, 6))
+#
+#exagonFollPath = obtainOffsetPath(exagonWall, 2, 6, vec(0, 0, 0))
+#squareFollPath = obtainOffsetPath(squareWall, 2, 4, vec(-4, 0, 0))
+#triangleFollPath = obtainOffsetPath(triangleWall, 4, 3, vec(0, 0, 6))
+#
+#wallPath = exagonWall + squareWall + triangleWall
 wall = extrusion(path=wallPath, shape=shapes.rectangle(width=offset, height=8))
+
+#pathPath = exagonFollPath + squareFollPath + triangleFollPath
+pathToFollow = extrusion(path=pathPath, shape=shapes.rectangle(width=offset, height=8))
+pathToFollow.opacity = 0
 
 wallFollower = Agent([10, 0, 10])
 agentSphere = wallFollower.generateSphere(radius)
@@ -84,7 +117,7 @@ previous = [0, 0, 0]
 
 while(True):
     points = []
-    for point in wallPath:
+    for point in pathPath:
         points.append([point.x, point.y, point.z])
         
     closestWallPoints = findClosests(wallFollower, points)
@@ -96,7 +129,7 @@ while(True):
     
     previous = copy.deepcopy(wallFollower.pos)
 
-    wallFollower.moveTo(closest, difference(closest, wallFollower.pos))
-    wallFollower.updateSphere(agentSphere)
+    wallFollower.moveTo(closest, difference(closest, wallFollower.pos), 0.1, agentSphere)
+#    wallFollower.updateSphere(agentSphere)
         
-    time.sleep(0.1)
+#    time.sleep(1)
